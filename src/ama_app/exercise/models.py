4f53de4516f323a77548db6da80bc076d92d05d8
ifkite <if.kite@gmail.com>
2016-07-10 13:21:59 +0800

    GET && POST to /api/v1/quesions

--- src/ama_app/exercise/models.py
+++ src/ama_app/exercise/models.py
@@ -1,7 +1,7 @@
 #! encoding=UTF-8
 from __future__ import unicode_literals
 
-from mongoengine import DynamicDocument, IntField, StringField, ListField, EmbeddedDocument, DateTimeField
+from mongoengine import DynamicDocument, IntField, StringField, ListField, EmbeddedDocument, DateTimeField, DynamicField
 from ama_app.exercise.excepts import ContentExcept, AnswerNotExistExcept
 import datetime
 import logging
@@ -23,21 +23,21 @@ class Choice(EmbeddedDocument):
 
 
 class Question(DynamicDocument):
-    CHOICE = 'CHOICE'
-    HIDE = 'HIDE'
-    EASY = 'EASY'
-    MEDIUM = 'MEDIUM'
-    HARD = 'HARD'
+    CHOICE = 'choice'
+    HIDE = 'hide'
+    EASY = 'easy'
+    MEDIUM = 'medium'
+    HARD = 'hard'
     Q_TYPE = (CHOICE, HIDE)
     LEVEL= (EASY, MEDIUM, HARD)
     Q_TYPE_DICT = {CHOICE: Choice}
 
-    type = StringField(choices=Q_TYPE, default=HIDE)
+    content_type = StringField(choices=Q_TYPE, default=HIDE)
     score = IntField(min_value=1, default=1)
-    # content = EmbeddedDocumentField(require=True)
+    content = DynamicField()
     # avg_time = IntField()
     level = StringField(choices=LEVEL, default=MEDIUM)
-    tag = StringField()
+    tag = StringField(default='')
     # TODO:store like, dislike, render_time, avg_time in redis
     # like =  IntField(min_value=0, default=0)
     # dislike =  IntField(min_value=0, default=0)
@@ -46,21 +46,23 @@ class Question(DynamicDocument):
     author = IntField()
     create_time = DateTimeField(default=datetime.datetime.utcnow())
     modify_time = DateTimeField(default=datetime.datetime.utcnow())
-    meta = {'collection': 'question',
-            'indexes':[
-                ('tag', 'rank'),
-                'type',
-            ],
-           }
+
+    meta = {
+                'collection': 'question',
+                'indexes':[
+                    ('tag', 'rank'),
+                    'content_type',
+                ],
+            }
 
     def __repr__(self):
-        return u'type<{0}> id<{0}>'.format(str(type(self)), str(self.id))
+        return unicode('type{0} id<{1}>'.format(str(type(self)), str(self.id)))
 
     def set_content(self, content_type, content_data):
         if content_type not in self.Q_TYPE_DICT:
             log.warning("set content faild: content type {0} not support".format(content_type))
             raise ContentExcept
-        self.type = content_type
+        self.content_type = content_type
         Content = self.Q_TYPE_DICT.get(content_type)
         content = Content()
         content.set_content(content_data)
--- src/ama_gunicorn.py
+++ src/ama_gunicorn.py
@@ -2,7 +2,7 @@ from os import environ
 from os.path import dirname, realpath
 
 timeout = 300
-bind = "127.0.0.1:8000"
+bind = "0.0.0.0:8848"
 pythonpath = environ.get('AMA_HOME', dirname(realpath(__file__)))
 daemon = False
 workers = 2
